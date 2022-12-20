0041c40bd845f1c28ebe60722ec712af27c3c3af
abaskurt <ali.han.baskurt@gmail.com>
2022-12-21 01:52:11 +0300

    Db Modeller oluşturuldu.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    internal class Category
+    {
+        [BsonId] //Mongodb de bu alanın Id oldugunu belirtiriz.
+        [BsonRepresentation(BsonType.ObjectId)] //Mongodb tarafında objectId tipinde gösterilir.
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Course.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    internal class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public string Price { get; set; }
+        public string Picture { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+        public string UserId { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+        public Feature Feature { get; set; }
+
+
+        [BsonIgnore]
+        public Category Category { get; set; }
+
+
+    }
+}
