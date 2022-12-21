94ed861b65b60f35dcdc3b13680fb4044011d64d
abaskurt <ali.han.baskurt@gmail.com>
2022-12-21 17:46:24 +0300

    appsetting içerisine mongodb collection, connecting string tanımlamaları yapılıp DatabaseSettings sınıfı için program.cs de configurasyonlar yapıldı.

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,3 +1,6 @@
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +13,16 @@ builder.Services.AddSwaggerGen();
 
 //Program.cs altýndaki Profile clasýn'ý inherit alan mapping class'larýný bulup IOC ye ekledim.
 builder.Services.AddAutoMapper(typeof(Program));
+
+//appsettings klasorundeki databasesetting property'si ile DatabaseSetting sýnýfýný doldurdum.
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+//Herhangi bir sýnýf yapýcý metodunda IDatabaseSettings geçersek DatabaseSettings sýnýftan bir nesne gelicek.
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
 
 var app = builder.Build();
 
