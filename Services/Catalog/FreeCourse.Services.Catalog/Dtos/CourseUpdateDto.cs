7a4bc2dd36402323558c7bb7f0e115e98ea2dd8b
abaskurt <ali.han.baskurt@gmail.com>
2022-12-23 16:48:02 +0300

    bug fix

--- Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseUpdateDto.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/CourseUpdateDto.cs
@@ -5,7 +5,7 @@
         public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Price { get; set; }
+        public decimal Price { get; set; }
         public string Picture { get; set; }
         public DateTime CreatedTime { get; set; }
         public string UserId { get; set; }
