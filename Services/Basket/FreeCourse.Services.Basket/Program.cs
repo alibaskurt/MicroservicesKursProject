53e9b84884266897611e8181a808f5223836e29a
abaskurt <ali.han.baskurt@gmail.com>
2022-12-27 15:00:02 +0300

    RedisSetting host ve port configurasyon ayarlamaları eklendi.

--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -1,6 +1,18 @@
+using FreeCourse.Services.Basket.Settings;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+//appsettings klasorundeki redissettings property'si ile redissettings sýnýfýný doldurdum.
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+
+//Herhangi bir sýnýf yapýcý metodunda IDatabaseSettings geçersek DatabaseSettings sýnýftan bir nesne gelicek.
+builder.Services.AddSingleton<IRedisSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
